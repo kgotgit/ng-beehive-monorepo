e23aa5c438720a94a17568558323de9264f36a58
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-05-19 09:02:38 -0500

    beehive red changes

--- projects/beehive-red/src/app/pages/bee-red-angry/bee-red-angry.module.ts
+++ projects/beehive-red/src/app/pages/bee-red-angry/bee-red-angry.module.ts
@@ -3,13 +3,15 @@ import { CommonModule } from '@angular/common';
 
 import { BeeRedAngryRoutingModule } from './bee-red-angry-routing.module';
 import { BeeRedAngryComponent } from './bee-red-angry.component';
+import { LibBeehiveUISharedModule } from 'projects/lib-beehive-ui-shared/src/public-api';
 
 
 @NgModule({
   declarations: [BeeRedAngryComponent],
   imports: [
     CommonModule,
-    BeeRedAngryRoutingModule
+    BeeRedAngryRoutingModule,
+    LibBeehiveUISharedModule
   ]
 })
 export class BeeRedAngryModule { }
