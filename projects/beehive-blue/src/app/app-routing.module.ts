305929e9b314325de76f2c61c1ccf3d7bbb56f05
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-05-18 19:09:28 -0500

    beehive blue changes

--- projects/beehive-blue/src/app/app-routing.module.ts
+++ projects/beehive-blue/src/app/app-routing.module.ts
@@ -2,7 +2,17 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    "path":"pages/beehive-red-happy",
+     loadChildren:()=>import('./pages/bee-blue-happy/bee-blue-happy.module').then(mod=>mod.BeeBlueHappyModule),
+  },
+  {"path":"",redirectTo:"pages/beehive-blue-happy",pathMatch:'full'},
+  {"path":"**",redirectTo:"pages/beehive-blue-happy",pathMatch:'full'},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/beehive-blue/src/app/pages/bee-blue-happy/bee-blue-happy-routing.module.ts
+++ projects/beehive-blue/src/app/pages/bee-blue-happy/bee-blue-happy-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { BeeBlueHappyComponent } from './bee-blue-happy.component';
 
 
 const routes: Routes = [
-
+  {path:'',component:BeeBlueHappyComponent}
 
 ];
 
