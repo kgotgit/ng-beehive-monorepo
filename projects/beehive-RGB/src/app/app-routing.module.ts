d7486d37f251367fd1fc8567fd47b5067bc814b1
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-05-18 22:39:09 -0500

    appcomponent changes

--- projects/beehive-RGB/src/app/app-routing.module.ts
+++ projects/beehive-RGB/src/app/app-routing.module.ts
@@ -2,7 +2,25 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    "path":"pages/beehive-red-happy",
+     loadChildren:()=>import('../../../beehive-red/src/app/pages/bee-red-happy/bee-red-happy.module').then(mod=>mod.BeeRedHappyModule),
+  },
+  {
+    "path":"pages/beehive-green-happy",
+     loadChildren:()=>import('../../../beehive-green/src/app/pages/bee-green-happy/bee-green-happy.module').then(mod=>mod.BeeGreenHappyModule),
+  },
+  {
+    "path":"pages/beehive-blue-happy",
+     loadChildren:()=>import('../../../beehive-blue/src/app/pages/bee-blue-happy/bee-blue-happy.module').then(mod=>mod.BeeBlueHappyModule),
+  },
+  {"path":"",redirectTo:"pages/beehive-red-happy",pathMatch:'full'},
+  {"path":"**",redirectTo:"pages/beehive-red-happy",pathMatch:'full'},
+
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
