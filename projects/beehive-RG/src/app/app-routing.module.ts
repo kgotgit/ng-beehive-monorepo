f2598fd2d337f0ea179e1d00d54d438e9cd9d138
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-05-19 20:49:37 -0500

    app routing module for beehive-RG

--- projects/beehive-RG/src/app/app-routing.module.ts
+++ projects/beehive-RG/src/app/app-routing.module.ts
@@ -2,7 +2,24 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    "path":"pages/beehive-red-happy",
+     loadChildren:()=>import('../../../beehive-red/src/app/pages/bee-red-happy/bee-red-happy.module').then(mod=>mod.BeeRedHappyModule),
+  },
+  {
+    "path":"pages/beehive-red-angry",
+     loadChildren:()=>import('../../../beehive-red/src/app/pages/bee-red-angry/bee-red-angry.module').then(mod=>mod.BeeRedAngryModule),
+  },
+  {
+    "path":"pages/beehive-green-happy",
+     loadChildren:()=>import('../../../beehive-green/src/app/pages/bee-green-happy/bee-green-happy.module').then(mod=>mod.BeeGreenHappyModule),
+  },
+  {
+    "path":"pages/beehive-green-cry",
+     loadChildren:()=>import('../../../beehive-green/src/app/pages/bee-green-cry/bee-green-cry.module').then(mod=>mod.BeeGreenCryModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- projects/beehive-RG/src/app/app.component.ts
+++ projects/beehive-RG/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { IMenu } from 'projects/lib-beehive-ui-shared/src/public-api';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'beehive-RG';
+
+
+
+  _menu:IMenu[];
+
+  ngOnInit(): void {
+    this._menu=[
+      {path:"pages/beehive-red-happy", name:"Red-Happy"} as IMenu,
+      {path:"pages/beehive-red-angry", name:"Red-Angry"} as IMenu,
+      {path:"pages/beehive-green-happy", name:"Green-Happy"} as IMenu,
+      {path:"pages/beehive-green-cry", name:"Green-Happy"} as IMenu,
+    ]
+  }
 }
