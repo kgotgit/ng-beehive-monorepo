5090b84dd14353f5629743360e512c75d3569348
karthikgotrala <6868408+kgotgit@users.noreply.github.com>
2020-05-19 20:33:48 -0500

    blue green changes

--- projects/beehive-RGB/src/app/app-routing.module.ts
+++ projects/beehive-RGB/src/app/app-routing.module.ts
@@ -8,12 +8,24 @@ const routes: Routes = [
      loadChildren:()=>import('../../../beehive-red/src/app/pages/bee-red-happy/bee-red-happy.module').then(mod=>mod.BeeRedHappyModule),
   },
   {
+    "path":"pages/beehive-red-angry",
+     loadChildren:()=>import('../../../beehive-red/src/app/pages/bee-red-angry/bee-red-angry.module').then(mod=>mod.BeeRedAngryModule),
+  },
+  {
     "path":"pages/beehive-green-happy",
      loadChildren:()=>import('../../../beehive-green/src/app/pages/bee-green-happy/bee-green-happy.module').then(mod=>mod.BeeGreenHappyModule),
   },
   {
+    "path":"pages/beehive-green-cry",
+     loadChildren:()=>import('../../../beehive-green/src/app/pages/bee-green-cry/bee-green-cry.module').then(mod=>mod.BeeGreenCryModule),
+  },
+  {
     "path":"pages/beehive-blue-happy",
      loadChildren:()=>import('../../../beehive-blue/src/app/pages/bee-blue-happy/bee-blue-happy.module').then(mod=>mod.BeeBlueHappyModule),
+  },
+  {
+    "path":"pages/beehive-blue-shock",
+     loadChildren:()=>import('../../../beehive-blue/src/app/pages/bee-blue-shock/bee-blue-shock.module').then(mod=>mod.BeeBlueShockModule),
   },
   {"path":"",redirectTo:"pages/beehive-red-happy",pathMatch:'full'},
   {"path":"**",redirectTo:"pages/beehive-red-happy",pathMatch:'full'},
--- projects/beehive-RGB/src/app/app.component.ts
+++ projects/beehive-RGB/src/app/app.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { IMenu } from 'projects/lib-beehive-ui-shared/src/public-api';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'beehive-RGB';
+
+
+  _menu:IMenu[];
+
+  ngOnInit(): void {
+    this._menu=[
+      {path:"pages/beehive-red-happy", name:"Red-Happy"} as IMenu,
+      {path:"pages/beehive-red-angry", name:"Red-Angry"} as IMenu,
+      {path:"pages/beehive-green-happy", name:"Green-Happy"} as IMenu,
+      {path:"pages/beehive-green-cry", name:"Green-Happy"} as IMenu,
+      {path:"pages/beehive-blue-happy", name:"Blue-Happy"} as IMenu,
+      {path:"pages/beehive-blue-shock", name:"Blue-Shock"} as IMenu,
+    ]
+  }
 }
